0f9df0eff0eb564d860980f2bbcaccc1d044edbb
mortrexe <136992611+mortrexe@users.noreply.github.com>
2023-06-18 18:58:31 +0200

    Update ping.js

--- src/Commands/Outils/ping.js
+++ src/Commands/Outils/ping.js
@@ -1,24 +1,49 @@
-const { MessageEmbed } = require('discord.js');
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 
 class command {
     constructor() {
-        this.name = "ping",
-        this.description = "Permets de voir le ping du bot."
+        this.name = "femmesalon",
+        this.description = "» Owner",
+        this.options = [
+        {
+            name: 'salon',
+            description: 'Salon que tu souhaite',
+            type: 'CHANNEL',
+            required: true
+        }]
+          
+            
     }
+ 
+    
 
     async execute(bot, interaction) {
-        const PING = new MessageEmbed()
-            .setColor(bot.config.clients.embedColor)
-            .setTitle('**Ping**')
-            .setDescription("🏓 Pong")
-            .addFields(
-                { name: '🔧 - Latence :', value: `${Date.now() - interaction.createdTimestamp}ms.` },
-            )
-            .setTimestamp()
-            .setFooter({ text: bot.config.clients.name, iconURL: bot.config.clients.logo});
+      const user = interaction.options.getChannel(`salon`)
+if(process.env.id == interaction.user.id ) {
+  
 
-        interaction.reply({ embeds: [PING] });
+        
+			
+  
+const eHelp = new MessageEmbed()
+    .setDescription(`_${user}_ a bien été ajouté salon femme.`)
+    .setColor("2f3136")
+      await db.set(`femmesalon_${interaction.guild.id}`, user.id).then(interaction.reply({ embeds:[eHelp]}))
+  
+        
+  } else {
+  const wl = new MessageEmbed()
+    .setTitle(":x: Tu ne peux pas !")
+    .setColor("2f3136")
+  
+  interaction.reply({embeds: [wl]})
+  }
     }
 }
+
+
+
 
 module.exports = command
